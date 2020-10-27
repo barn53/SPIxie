06d75a9cbf7cfae92c85a4e617d8147f1f92ecb3
Gerd Müller <gerdmuller.de@gmail.com>
2020-10-27 23:05:31 +0100

    it runs on esp8285 board!

---
+++ source/boards/readme.md
@@ -0,0 +1,2 @@
+# A good source for this topic:
+https://dokk.org/documentation/platformio/v3.6.0/platforms/espressif8266/#
--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,50 +1,134 @@
-#include <Arduino.h> // Hardware-specific library
-// Created by Bodmer 15/11/18 as an example for the TFT_eFEX graphics extension library to TFT_eSPI
+#include <Arduino.h>
 
+//   Diagnostic test for the displayed colour order
+//
+// Writen by Bodmer 17/2/19 for the TFT_eSPI library:
 // https://github.com/Bodmer/TFT_eSPI
-#include <TFT_eSPI.h> // Include the graphics library (this includes the sprite functions)
 
-// https://github.com/Bodmer/TFT_eFEX
-#include <TFT_eFEX.h> // Include the extension graphics functions library
+/* 
+ Different hardware manufacturers use different colour order
+ configurations at the hardware level.  This may result in
+ incorrect colours being displayed.
 
-TFT_eSPI tft = TFT_eSPI(); // Create object "tft"
+ Incorrectly displayed colours could also be the result of
+ using the wrong display driver in the library setup file.
 
-TFT_eFEX fex = TFT_eFEX(&tft); // Create TFT_eFEX object "fex" with pointer to "tft" object
+ Typically displays have a control register (MADCTL) that can
+ be used to set the Red Green Blue (RGB) colour order to RGB
+ or BRG so that red and blue are swapped on the display.
 
-// =========================================================================
-void setup(void)
-{
-    Serial.begin(115200);
-    Serial.printf("Kuckuck!\n");
+ This control register is also used to manage the display
+ rotation and coordinate mirroring. The control register
+ typically has 8 bits, for the ILI9341 these are:
 
-    tft.init();
-    tft.setRotation(1);
+ Bit Function
+ 7   Mirror Y coordinate (row address order)
+ 6   Mirror X coordinate (column address order)
+ 5   Row/column exchange (for rotation)
+ 4   Refresh direction (top to bottom or bottom to top in portrait orientation)
+ 3   RGB order (swaps red and blue)
+ 2   Refresh direction (top to bottom or bottom to top in landscape orientation)
+ 1   Not used
+ 0   Not used
+
+ The control register bits can be written with this example command sequence:
+ 
+    tft.writecommand(TFT_MADCTL);
+    tft.writedata(0x48);          // Bits 6 and 3 set
+    
+ 0x48 is the default value for ILI9341 (0xA8 for ESP32 M5STACK)
+ in rotation 0 orientation.
+ 
+ Another control register can be used to "invert" colours,
+ this swaps black and white as well as other colours (e.g.
+ green to magenta, red to cyan, blue to yellow).
+ 
+ To invert colours insert this line after tft.init() or tft.begin():
+
+    tft.invertDisplay( invert ); // Where invert is true or false
+
+*/
+
+#include <SPI.h>
+
+#include <TFT_eSPI.h>       // Hardware-specific library
+
+TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
+
+void setup(void) {
+  tft.init();
+  tft.setRotation(2);
+
+  tft.fillScreen(TFT_BLACK);
+  
+  // Set "cursor" at top left corner of display (0,0) and select font 4
+  tft.setCursor(0, 0, 2);
+
+  // Set the font colour to be white with a black background
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  // We can now plot text on screen using the "print" class
+  tft.println("Intialised default\n");
+  tft.println("White text");
+
+  tft.setTextColor(TFT_RED, TFT_BLACK);
+  tft.println("Red text");
+  
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println("Green text");
+  
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.println("Blue text");
+
+  delay(5000);
+
 }
 
-// =========================================================================
-void loop()
-{
-    tft.fillScreen(TFT_BLACK);
+void loop() {
 
-    uint16_t xc = 60;
-    uint16_t yb = 120;
-    uint16_t yh = 100 - random(100);
-    uint16_t color = random(0x10000);
+  tft.invertDisplay( false ); // Where i is true or false
+ 
+  tft.fillScreen(TFT_BLACK);
+  
+  tft.setCursor(0, 0, 2);
 
-    // This function exists only in the TFT_eFEX library
-    fex.drawBezier(xc - 55, yb, xc, yb - yh, xc + 55, yb, color);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.println("Invert OFF\n");
 
-    // Draw in two segments with ends at p0 and p2, peak at p1
-    fex.drawBezierSegment(xc - 35, yb, xc - 20, yb - yh, xc, yb - yh, color);
-    fex.drawBezierSegment(xc + 35, yb, xc + 20, yb - yh, xc, yb - yh, color);
+  tft.println("White text");
+  
+  tft.setTextColor(TFT_RED, TFT_BLACK);
+  tft.println("Red text");
+  
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println("Green text");
+  
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.println("Blue text");
 
-    // The TFT_eFEX library "fex" object has inherited the graphics functions in the TFT_eSPI library
-    fex.fillRect(0, 0, 20, 20, TFT_RED); // fillRect does not exist in the TFT_eFEX library
+  delay(5000);
 
-    // The "tft" object function calls still work and make it clearer where the function resides
-    tft.fillRect(20, 0, 20, 20, TFT_GREEN);
-    tft.fillRect(40, 0, 20, 20, TFT_BLUE);
 
-    delay(2000);
-    Serial.printf("Kuckuck!\n");
+  // Binary inversion of colours
+  tft.invertDisplay( true ); // Where i is true or false
+ 
+  tft.fillScreen(TFT_BLACK);
+  
+  tft.setCursor(0, 0, 2);
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.println("Invert ON\n");
+
+  tft.println("White text");
+  
+  tft.setTextColor(TFT_RED, TFT_BLACK);
+  tft.println("Red text");
+  
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println("Green text");
+  
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.println("Blue text");
+
+  delay(5000);
 }
