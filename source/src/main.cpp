3ba5590aa7279897722a3d03e08fa9c4dbdbc2d3
Gerd Müller <ge.mueller@sap.com>
2020-12-22 15:39:06 +0100

    added stuff

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,134 +1,76 @@
 #include <Arduino.h>
+/*====================================================================================
 
-//   Diagnostic test for the displayed colour order
-//
-// Writen by Bodmer 17/2/19 for the TFT_eSPI library:
-// https://github.com/Bodmer/TFT_eSPI
+           Test jpeg rendering with ESP8266 and JPEGDecoder library
 
-/* 
- Different hardware manufacturers use different colour order
- configurations at the hardware level.  This may result in
- incorrect colours being displayed.
+  ==================================================================================*/
 
- Incorrectly displayed colours could also be the result of
- using the wrong display driver in the library setup file.
+// The Jpeg library required can be found here:
+// https://github.com/Bodmer/JPEGDecoder
 
- Typically displays have a control register (MADCTL) that can
- be used to set the Red Green Blue (RGB) colour order to RGB
- or BRG so that red and blue are swapped on the display.
+// The example images used to test this sketch can be found in the sketch
+// Data folder, press Ctrl+K to see this folder. Use the IDE "Tools" menu
+// "ESP8266 Sketch Data Upload" option to upload the images to SPIFFS.
 
- This control register is also used to manage the display
- rotation and coordinate mirroring. The control register
- typically has 8 bits, for the ILI9341 these are:
-
- Bit Function
- 7   Mirror Y coordinate (row address order)
- 6   Mirror X coordinate (column address order)
- 5   Row/column exchange (for rotation)
- 4   Refresh direction (top to bottom or bottom to top in portrait orientation)
- 3   RGB order (swaps red and blue)
- 2   Refresh direction (top to bottom or bottom to top in landscape orientation)
- 1   Not used
- 0   Not used
-
- The control register bits can be written with this example command sequence:
- 
-    tft.writecommand(TFT_MADCTL);
-    tft.writedata(0x48);          // Bits 6 and 3 set
-    
- 0x48 is the default value for ILI9341 (0xA8 for ESP32 M5STACK)
- in rotation 0 orientation.
- 
- Another control register can be used to "invert" colours,
- this swaps black and white as well as other colours (e.g.
- green to magenta, red to cyan, blue to yellow).
- 
- To invert colours insert this line after tft.init() or tft.begin():
-
-    tft.invertDisplay( invert ); // Where invert is true or false
-
-*/
+//====================================================================================
+//                                  Libraries
+//====================================================================================
+// Call up the SPIFFS FLASH filing system this is part of the ESP Core
+#define FS_NO_GLOBALS
+#include <FS.h>
 
 #include <SPI.h>
 
-#include <TFT_eSPI.h>       // Hardware-specific library
+// https://github.com/Bodmer/TFT_eSPI
+#include <TFT_eSPI.h> // Hardware-specific library
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
-TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
+// https://github.com/Bodmer/TFT_eFEX
+#include <TFT_eFEX.h> // Include the extension graphics functions library
+TFT_eFEX fex = TFT_eFEX(&tft); // Create TFT_eFX object "efx" with pointer to "tft" object
 
-void setup(void) {
-  tft.init();
-  tft.setRotation(2);
+//====================================================================================
+//                                    Setup
+//====================================================================================
+void setup()
+{
+    Serial.begin(115200); // Used for messages and the C array generator
 
-  tft.fillScreen(TFT_BLACK);
-  
-  // Set "cursor" at top left corner of display (0,0) and select font 4
-  tft.setCursor(0, 0, 2);
+    delay(10);
+    Serial.println("\nESP8266 Jpeg decoder test!");
 
-  // Set the font colour to be white with a black background
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.begin();
 
-  // We can now plot text on screen using the "print" class
-  tft.println("Intialised default\n");
-  tft.println("White text");
+    tft.setRotation(2); // 0 & 2 Portrait. 1 & 3 landscape
 
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.println("Red text");
-  
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.println("Green text");
-  
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.println("Blue text");
+    tft.fillScreen(TFT_BLACK);
 
-  delay(5000);
+    if (!SPIFFS.begin()) {
+        Serial.println("SPIFFS initialisation failed!");
+        while (1)
+            yield(); // Stay here twiddling thumbs waiting
+    }
+    Serial.println("\r\nInitialisation done.");
 
+    fex.listSPIFFS(); // Lists the files so you can see what is in the SPIFFS
 }
 
-void loop() {
+char filename[20];
 
-  tft.invertDisplay( false ); // Where i is true or false
- 
-  tft.fillScreen(TFT_BLACK);
-  
-  tft.setCursor(0, 0, 2);
-
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.println("Invert OFF\n");
-
-  tft.println("White text");
-  
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.println("Red text");
-  
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.println("Green text");
-  
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.println("Blue text");
-
-  delay(5000);
-
-
-  // Binary inversion of colours
-  tft.invertDisplay( true ); // Where i is true or false
- 
-  tft.fillScreen(TFT_BLACK);
-  
-  tft.setCursor(0, 0, 2);
-
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.println("Invert ON\n");
-
-  tft.println("White text");
-  
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  tft.println("Red text");
-  
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.println("Green text");
-  
-  tft.setTextColor(TFT_BLUE, TFT_BLACK);
-  tft.println("Blue text");
-
-  delay(5000);
+//====================================================================================
+//                                    Loop
+//====================================================================================
+void loop()
+{
+    fex.drawJpeg("/b.jpg", 0, 0);
+    delay(1000);
+    for (auto t : { 1, 2 }) {
+        for (auto n : { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }) {
+            snprintf(filename, sizeof(filename), "/%u/%u.jpg", t, n);
+            Serial.printf("File: '%s'\n", filename);
+            fex.drawJpeg(filename, 0, 0);
+            delay(1000);
+        }
+    }
 }
+//====================================================================================
